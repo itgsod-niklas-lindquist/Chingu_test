5a9898f1f97b1f0a365ad092ee8258d4d2c8c877
Niklas Lindquist <Niklas.lindquist@elev.it-gymnasiet.se>
2012-11-23 10:23:52 +0100

    Added Player class

--- gosu.rb
+++ gosu.rb
@@ -6,6 +6,18 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		@player = Player.create
+	end
+
+end
+
+class Player < Chingu::GameObject
+
+	#meta-contructor
+	def setup
+		@x, @y = 350, 400
+		@image = Gosu::Image["bullshit.psd"]  #Skeppet
+
 	end
 
 end
